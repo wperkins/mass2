689b235552111e21aaab7140cbcdc0c7302eea24
William Perkins <william.perkins@pnnl.gov>
2003-04-04 16:07:58 +0000

    Minor changes to autotools configuration.

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,8 +8,10 @@
 # -------------------------------------------------------------
 # -------------------------------------------------------------
 # Created March 26, 2003 by William A. Perkins
-# Last Change: Fri Mar 28 13:59:42 2003 by William A. Perkins <perk@leechong.pnl.gov>
+# Last Change: Wed Apr  2 07:16:47 2003 by William A. Perkins <perk@leechong.pnl.gov>
 # -------------------------------------------------------------
 # $Id$
 
 aclocal -I time_series  && automake --foreign --add-missing --copy && autoconf
+
+(cd time_series; sh bootstrap.sh)
