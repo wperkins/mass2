9bf351d65ba210e9fc4021913bdf61c052f9406f
William Perkins <william.perkins@pnnl.gov>
2009-05-27 15:30:26 +0000

    (gage_file_header): added (gage_print_text): revive text gage output and make default

--- gage_output_module.f90
+++ gage_output_module.f90
@@ -45,7 +45,7 @@ MODULE gage_output
   INTEGER :: num_gages
   CHARACTER (LEN=80), PARAMETER, PRIVATE :: gage_control = 'gage_control.dat'
 
-  LOGICAL, PARAMETER, PRIVATE :: gage_do_text = .FALSE.
+  LOGICAL, PARAMETER, PRIVATE :: gage_do_text = .TRUE.
   LOGICAL, PARAMETER, PRIVATE :: gage_do_netcdf = (.NOT. gage_do_text)
 
   INTEGER, PRIVATE :: gage_ncid
@@ -85,7 +85,7 @@ CONTAINS
        gage_rec%ident = buffer
     END IF
 
-100 FORMAT('block=', I2.2, ' i=', I3.3, ' j=', I3.3)
+100 FORMAT('block=', I2.2, ',i=', I3.3, ',j=', I3.3)
   END SUBROUTINE gage_make_ident
   
 
@@ -138,76 +138,89 @@ CONTAINS
 
   END SUBROUTINE gage_read_control
 
+  ! ----------------------------------------------------------------
+  ! SUBROUTINE gage_file_header
+  ! ----------------------------------------------------------------
+  SUBROUTINE gage_file_header(iounit)
 
-  !##################################################################################
-  SUBROUTINE gage_file_setup_text(error_iounit, status_iounit)
+    USE globals
+    USE scalars, ONLY: max_species
+    USE scalars_source
+    USE bed_module
+    USE misc_vars, ONLY: do_transport
 
     IMPLICIT NONE
 
-    INTEGER :: i, error_iounit, status_iounit, alloc_stat
-    INTEGER :: len1,len2,len3,spot1,spot2,spot3
-    CHARACTER*20 string1,string2,string3
-    CHARACTER*80 stringall
+    INTEGER, INTENT(IN) :: iounit
+    INTEGER :: i
 
-    gage_specs%filename = ''
+    WRITE(iounit, 101, ADVANCE='NO') "timestamp"
+    WRITE(iounit, 100, ADVANCE='NO') "elapsed"
+    
+    ! time-dependent data variables
+
+    WRITE(iounit, 100, ADVANCE='NO') "wsel"
+    WRITE(iounit, 100, ADVANCE='NO') "depth"
+    WRITE(iounit, 100, ADVANCE='NO') "vmag"
+    WRITE(iounit, 100, ADVANCE='NO') "uvel"
+    WRITE(iounit, 100, ADVANCE='NO') "vvel"
+    WRITE(iounit, 100, ADVANCE='NO') "isdry"
+
+    IF (do_transport) THEN
+       DO i = 1, max_species
+          WRITE(iounit, 100, ADVANCE='NO') TRIM(scalar_source(i)%name)
+          
+          SELECT CASE (scalar_source(i)%srctype)
+          CASE (GEN)
+             IF (source_doing_sed) THEN
+                WRITE(iounit, 100, ADVANCE='NO') TRIM(scalar_source(i)%name) // '-pore'
+                WRITE(iounit, 100, ADVANCE='NO') TRIM(scalar_source(i)%name) // '-bed'
+                WRITE(iounit, 100, ADVANCE='NO') TRIM(scalar_source(i)%name) // '-bedmass'
+             END IF
+          CASE (PART)
+             WRITE(iounit, 100, ADVANCE='NO') TRIM(scalar_source(i)%name) // '-depos'
+             WRITE(iounit, 100, ADVANCE='NO') TRIM(scalar_source(i)%name) // '-bedmass'
+             WRITE(iounit, 100, ADVANCE='NO') TRIM(scalar_source(i)%name) // '-bed'
+          CASE (TDG)                             
+             WRITE(iounit, 100, ADVANCE='NO') "tdgpress"
+             WRITE(iounit, 100, ADVANCE='NO') "tdgdeltap"
+             WRITE(iounit, 100, ADVANCE='NO') "tdgsat"
+          CASE (SED)                             
+             WRITE(iounit, 100, ADVANCE='NO') TRIM(scalar_source(i)%name) // '-depos'
+             WRITE(iounit, 100, ADVANCE='NO') TRIM(scalar_source(i)%name) // '-erode'
+             WRITE(iounit, 100, ADVANCE='NO') TRIM(scalar_source(i)%name) // '-bedmass'
+             WRITE(iounit, 100, ADVANCE='NO') TRIM(scalar_source(i)%name) // '-bed'
+          END SELECT
+       END DO
+       IF (source_doing_sed) THEN
+          WRITE(iounit, 100, ADVANCE='NO') "beddepth"
+       END IF
+    END IF
+
+    WRITE(iounit, *)
+  
+100 FORMAT(A15, ',')
+101 FORMAT(A20, ',')
+  END SUBROUTINE gage_file_header
+
+
+
+  !##################################################################################
+  SUBROUTINE gage_file_setup_text(do_transport)
+
+    IMPLICIT NONE
+
+    LOGICAL, INTENT(IN) :: do_transport
+    INTEGER :: i
 
     ! construct the generic file name for the gage output files
     ! file = "gage_block=1_icell=11_jcell=2.out"
     DO i=1,num_gages
-       stringall = ''
-       ! gage_specs(i)%filename(1:11) = "gage_block="
-       stringall(1:11) = "gage_block="
-       WRITE(string1,*)gage_specs(i)%block
-       WRITE(string2,*)gage_specs(i)%i_cell
-       WRITE(string3,*)gage_specs(i)%j_cell
-       !READ(gage_point(i),*)string2
-       string1 = ADJUSTL(string1)
-       string2 = ADJUSTL(string2)
-       string3 = ADJUSTL(string3)
-       len1 = LEN_TRIM(string1)
-       len2 = LEN_TRIM(string2)
-       len3 = LEN_TRIM(string3)
-       spot1 =12 + len1 - 1
-       ! gage_specs(i)%filename(12:spot1) = string1(1:len1)
-       stringall(12:spot1) = string1(1:len1)
-       spot1 = spot1 + 1
-       spot2 = spot1 + LEN_TRIM("_icell=") - 1
-       ! gage_specs(i)%filename(spot1:spot2) = "_icell="
-       stringall(spot1:spot2) = "_icell="
-       spot1 = spot2 + 1
-       spot2 = spot1 + len2 - 1
-       ! gage_specs(i)%filename(spot1:spot2) = string2(1:len2)
-       stringall(spot1:spot2) = string2(1:len2)
-       spot1 = spot2 + 1
-       spot2 = spot1 + LEN_TRIM("_jcell=") - 1
-       ! gage_specs(i)%filename(spot1:spot2) = "_jcell="
-       stringall(spot1:spot2) = "_jcell="
-       spot1 = spot2 + 1
-       spot2 = spot1 + len3 - 1
-       ! gage_specs(i)%filename(spot1:spot2) = string3(1:len3)
-       stringall(spot1:spot2) = string3(1:len3)
-       spot1 = spot2 + 1
-       spot2 = spot1 + LEN_TRIM(".out")
-       ! gage_specs(i)%filename(spot1:spot2) = '.out'
-       stringall(spot1:spot2) = '.out'
-       gage_specs(i)%filename = stringall
-    END DO
-    
-    ! open the files and write out the header info
-    DO i=1,num_gages
-       OPEN(50,file=gage_specs(i)%filename)
-       WRITE(50,1015)gage_specs(i)%block, gage_specs(i)%i_cell, gage_specs(i)%j_cell
-       WRITE(50,1020)
-       WRITE(50,1005)
-1005   FORMAT('#date',8x,'time',5x,'hours from start',5x,'water elev',2x,'depth',5x,'vel mag',5x,'u-cart vel',4x,'v-cart vel', &
-            7x,'Block Mass Source',2x,'species 1 - TDG (mg/l) ',2x,'species 2 - Temp (C)', &
-            2x,'Total Diss. Gas Press. (mmHg)',2x,'TDG deltaP (mmHg)',2x,'TDG %Sat')
-       WRITE(50,1020)
+       gage_specs(i)%filename = "gage_" // TRIM(gage_specs(i).ident) // ".out"
+       OPEN(gage_iounit, file=gage_specs(i)%filename)
+       CALL gage_file_header(gage_iounit)
        CLOSE(50)
-       
     END DO
-1020 FORMAT('#',80('-'))
-1015 FORMAT('#gage output for Block ',i5,' i cell =',i5,' j cell=',i5)
   END SUBROUTINE gage_file_setup_text
   
   ! ----------------------------------------------------------------
@@ -218,58 +231,118 @@ CONTAINS
 
     USE globals
     USE scalars
+    USE date_time
     USE gas_functions
+    USE scalars_source
+    USE bed_module
    
     IMPLICIT NONE
-    CHARACTER*(*) :: date_string, time_string
-    DOUBLE PRECISION :: elapsed, salinity, baro_press
-    LOGICAL :: do_transport
-    INTEGER i, ispecies, iblock, icell, jcell
-    DOUBLE PRECISION :: species_io_vec(100)
+    CHARACTER*(*), INTENT(IN) :: date_string, time_string
+    DOUBLE PRECISION, INTENT(IN) :: elapsed, salinity, baro_press
+    LOGICAL, INTENT(IN) :: do_transport
+
+    INTEGER :: i, j, iblock, icell, jcell, ifract
     DOUBLE PRECISION :: conc_TDG, t_water
+    DOUBLE PRECISION :: value
+    CHARACTER (LEN=tslen) :: timestamp
+
+    timestamp = TRIM(date_string) // ' ' // TRIM(time_string)
 
     DO i=1,num_gages
-       OPEN(50,file=gage_specs(i)%filename, POSITION="APPEND")
+       OPEN(gage_iounit, file=gage_specs(i)%filename, POSITION="APPEND")
+
+       WRITE(gage_iounit, 101, ADVANCE='NO') timestamp
+       WRITE(gage_iounit, 100, ADVANCE='NO') elapsed
+
        iblock = gage_specs(i)%block
        icell = gage_specs(i)%i_cell + 1 ! convert from cell to i,j
        jcell = gage_specs(i)%j_cell + 1
 
-                                ! need to fix this for transport
-
-!        IF(do_transport)THEN
-!           DO ispecies=1,max_species
-!              species_io_vec(ispecies) = &
-!                   &species(ispecies)%scalar(iblock)%conc(icell,jcell)
-!           END DO
-!           conc_TDG = species(1)%scalar(iblock)%conc(icell,jcell)
-!           t_water = species(2)%scalar(iblock)%conc(icell,jcell)
-
-!           WRITE(50,100)date_string,time_string, elapsed, &
-!                block(iblock)%wsel(icell,jcell),block(iblock)%depth(icell,jcell), &
-!                SQRT(block(iblock)%uvel(icell,jcell)**2 + block(iblock)%vvel(icell,jcell)**2), &
-!                block(iblock)%uvel(icell,jcell), block(iblock)%vvel(icell,jcell), &
-!                SUM(ABS(block(iblock)%mass_source)), &
-!                species_io_vec(1:max_species), &
-!                TDGasPress(conc_TDG,  t_water,  salinity), &
-!                TDGasDP(conc_TDG, t_water,  salinity,  baro_press), &
-!                TDGasSaturation( conc_TDG, t_water,  salinity, baro_press)
-!        ELSE 
-          species_io_vec(1:max_species) = 0
-          WRITE(50,100)date_string,time_string, elapsed, &
-               block(iblock)%wsel(icell,jcell),block(iblock)%depth(icell,jcell), &
-               SQRT(block(iblock)%uvel(icell,jcell)**2 + block(iblock)%vvel(icell,jcell)**2), &
-               block(iblock)%uvel(icell,jcell), block(iblock)%vvel(icell,jcell), &
-               SUM(ABS(block(iblock)%mass_source)), &
-               species_io_vec(1:max_species), &
-               0.0, &
-               0.0, &
-               0.0
-!       END IF
        
+       WRITE (gage_iounit, 102, ADVANCE='NO') block(iblock)%wsel(icell,jcell)
+       WRITE (gage_iounit, 102, ADVANCE='NO') block(iblock)%depth(icell,jcell)
+       WRITE (gage_iounit, 102, ADVANCE='NO') &
+            &SQRT(block(iblock)%uvel(icell,jcell)**2 + block(iblock)%vvel(icell,jcell)**2)
+       WRITE (gage_iounit, 102, ADVANCE='NO') block(iblock)%uvel(icell,jcell)
+       WRITE (gage_iounit, 102, ADVANCE='NO') block(iblock)%vvel(icell,jcell)
 
+       IF (block(iblock)%isdry(icell,jcell)) THEN
+          value = 1.0
+       ELSE
+          value = 0.0
+       END IF
+       WRITE (gage_iounit, 102, ADVANCE='NO') value
+
+       IF(do_transport)THEN
+
+          IF (source_doing_temp) THEN
+             t_water = species(source_temp_idx)%scalar(iblock)%conc(icell,jcell)
+          END IF
+          DO j = 1, max_species
+             WRITE (gage_iounit, 102, ADVANCE='NO') &
+                  &species(j)%scalar(iblock)%conc(icell,jcell)/&
+                  &scalar_source(j)%conversion
+             SELECT CASE(scalar_source(j)%srctype)
+             CASE (GEN)
+                IF (scalar_source(j)%generic_param%issorbed) THEN
+                   WRITE(gage_iounit, 102, ADVANCE='NO') bed(iblock)%pore(j, icell, jcell)
+                   value = 0.0
+                   IF (bed(iblock)%depth(icell, jcell) .GT. 0.0) value= &
+                        &bed(iblock)%pore(j, icell, jcell)/ &
+                        &(bed(iblock)%depth(icell, jcell)* &
+                        &bed(iblock)%porosity(icell, jcell))
+                   WRITE(gage_iounit, 102, ADVANCE='NO') value
+                   WRITE(gage_iounit, 102, ADVANCE='NO') &
+                        &bed(iblock)%pore(j, icell, jcell)*&
+                        &block(iblock)%hp1(icell, jcell)*block(iblock)%hp2(icell, jcell)
+                ELSE
+                   value = 0.0
+                   WRITE(gage_iounit, 102, ADVANCE='NO') value
+                   WRITE(gage_iounit, 102, ADVANCE='NO') value
+                   WRITE(gage_iounit, 102, ADVANCE='NO') value
+                END IF
+                
+             CASE (TDG)                             
+                conc_TDG = species(j)%scalar(iblock)%conc(icell,jcell)
+                value = TDGasPress(conc_TDG,  t_water,  salinity)
+                WRITE(gage_iounit, 102, ADVANCE='NO') value
+                
+                value = TDGasDP(conc_TDG,  t_water,  salinity, baro_press)
+                WRITE(gage_iounit, 102, ADVANCE='NO') value
+          
+                value = TDGasSaturation(conc_TDG,  t_water,  salinity, baro_press)
+                WRITE(gage_iounit, 102, ADVANCE='NO') value
+
+             CASE (SED)                             
+                ifract = scalar_source(j)%sediment_param%ifract
+                WRITE(gage_iounit, 102, ADVANCE='NO')&
+                     &scalar_source(j)%sediment_param%block(iblock)%deposition(icell, jcell)
+                WRITE(gage_iounit, 102, ADVANCE='NO')&
+                     &scalar_source(j)%sediment_param%block(iblock)%erosion(icell, jcell)
+                WRITE(gage_iounit, 102, ADVANCE='NO')&
+                     &bed(iblock)%sediment(ifract, icell, jcell)* &
+                     &block(iblock)%hp1(icell, jcell)*block(iblock)%hp2(icell, jcell)
+                WRITE(gage_iounit, 102, ADVANCE='NO')&
+                     &bed(iblock)%sediment(ifract, icell, jcell)
+
+             CASE (PART)
+                WRITE(gage_iounit, 102, ADVANCE='NO')&
+                     &scalar_source(j)%part_param%block(iblock)%bedexch(icell, jcell)
+                WRITE(gage_iounit, 102, ADVANCE='NO')&
+                     &bed(iblock)%sediment(j, icell, jcell)* &
+                     &block(iblock)%hp1(icell, jcell)*block(iblock)%hp2(icell, jcell)
+                WRITE(gage_iounit, 102, ADVANCE='NO')&
+                     &bed(iblock)%particulate(j, icell, jcell)
+                
+             END SELECT
+
+          END DO
+       END IF
        CLOSE(50)
     END DO
-100 FORMAT(a10,2x,a8,2x,50(f12.2,2x))
+100 FORMAT(F15.6, ',')
+101 FORMAT(A20, ',')
+102 FORMAT(E15.8, ',')
 
   END SUBROUTINE gage_print_text
 
@@ -848,7 +921,7 @@ CONTAINS
 
     IF (num_gages .gt. 0) THEN
        IF (gage_do_text) THEN
-          CALL gage_file_setup_text(error_iounit, status_iounit)
+          CALL gage_file_setup_text(do_transport)
        END IF
        IF (gage_do_netcdf) THEN
           CALL gage_file_setup_netcdf(do_transport)
