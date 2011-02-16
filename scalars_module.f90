7d6ee8a590bc99a5604c4598766f61b90a3898ef
William Perkins <william.perkins@pnnl.gov>
2011-02-16 23:48:06 +0000

    reworked to use block_variable to store concentrations

--- scalars_module.f90
+++ scalars_module.f90
@@ -25,176 +25,199 @@ MODULE scalars
 
   USE utility
   USE globals
+  USE config, ONLY: max_blocks, max_species
+  USE block_variable
 
-IMPLICIT NONE
+  IMPLICIT NONE
 
-INTEGER  :: max_species
+  ! a list of cell types
 
-                                ! a list of cell types
+  INTEGER, PUBLIC, PARAMETER :: &
+       &SCALAR_NORMAL_TYPE = 1, &
+       &SCALAR_BOUNDARY_TYPE = 2
 
-INTEGER, PUBLIC, PARAMETER :: &
-     &SCALAR_NORMAL_TYPE = 1, &
-     &SCALAR_BOUNDARY_TYPE = 2
-                                ! a list of scalar boundary conditions
-                                ! types
-INTEGER, PUBLIC, PARAMETER :: &
-     &SCALBC_NONE = 0, &
-     &SCALBC_ZG = 1, &
-     &SCALBC_CONC = 2, &
-     &SCALBC_BLOCK = 3
+  ! a list of scalar boundary conditions
+  ! types
+  INTEGER, PUBLIC, PARAMETER :: &
+       &SCALBC_NONE = 0, &
+       &SCALBC_ZG = 1, &
+       &SCALBC_CONC = 2, &
+       &SCALBC_BLOCK = 3
 
-TYPE scalar_cell_type_struct
-   INTEGER :: xtype, ytype
-   ! the rest applies only for BOUNDARY type cells
-   INTEGER :: xbctype, ybctype
-END TYPE scalar_cell_type_struct
+  TYPE scalar_cell_type_struct
+     INTEGER :: xtype, ytype
+     ! the rest applies only for BOUNDARY type cells
+     INTEGER :: xbctype, ybctype
+  END TYPE scalar_cell_type_struct
 
-TYPE scalar_struct
-	DOUBLE PRECISION, POINTER :: conc(:,:) ! c depth-ave concentration
-	DOUBLE PRECISION, POINTER :: concold(:,:) ! c old depth-ave concentration
-	DOUBLE PRECISION, POINTER :: concoldold(:,:) ! c old depth-ave concentration
-    DOUBLE PRECISION, POINTER :: srcterm(:,:) ! precomputed source term
-    TYPE (scalar_cell_type_struct), POINTER :: cell(:,:)
+  TYPE scalar_struct
+     TYPE (block_var), POINTER :: concvar
+     DOUBLE PRECISION, POINTER :: conc(:,:)
+     DOUBLE PRECISION, POINTER :: concold(:,:)
+     DOUBLE PRECISION, POINTER :: concoldold(:,:)
+     DOUBLE PRECISION, POINTER :: srcterm(:,:)
 
-                                ! keep track of the mass of the
-                                ! species in the water column and in
-                                ! the bed
-    DOUBLE PRECISION :: mass, massold
-    DOUBLE PRECISION :: bedmass, bedmassold
+     TYPE (scalar_cell_type_struct), POINTER :: cell(:,:)
 
-                                ! a place to record the accumulated
-                                ! flux of the species into the box
-                                ! (negative means outflow)
-    DOUBLE PRECISION :: influx, outflux, netflux
-END TYPE scalar_struct
+     ! keep track of the mass of the
+     ! species in the water column and in
+     ! the bed
+     DOUBLE PRECISION :: mass, massold
+     DOUBLE PRECISION :: bedmass, bedmassold
 
-TYPE species_struct
+     ! a place to record the accumulated
+     ! flux of the species into the box
+     ! (negative means outflow)
+     DOUBLE PRECISION :: influx, outflux, netflux
+  END TYPE scalar_struct
 
-	TYPE(scalar_struct), POINTER :: scalar(:) ! need one for each block up to max_blocks
+  TYPE species_struct
+     TYPE(scalar_struct), POINTER :: scalar(:) ! need one for each block up to max_blocks
+  END TYPE species_struct
 
-END TYPE species_struct
+  TYPE(species_struct), POINTER :: species(:)
 
-
-TYPE(species_struct), ALLOCATABLE :: species(:)
-
-CHARACTER (LEN=1024), PRIVATE :: msg
+  CHARACTER (LEN=1024), PRIVATE :: msg
 
 
 CONTAINS
-!#####################################################################################################
 
-SUBROUTINE allocate_scalarblock_components(i, block, xmax, ymax)
-  ! this routine allocates each component in the array of blocks
-  ! allows minimal memory use for each block
-  IMPLICIT NONE
-  INTEGER :: block, i, alloc_stat	
-  INTEGER :: xmax, ymax, imin, imax, jmin, jmax
+  ! ----------------------------------------------------------------
+  ! SUBROUTINE allocate_species
+  ! ----------------------------------------------------------------
+  SUBROUTINE allocate_species()
 
-  imin = i_index_min
-  imax = xmax + i_index_extra
-  jmin = j_index_min
-  jmax = ymax + j_index_extra
-
-  WRITE(msg,*)'starting component allocation for scalars block number - ', block
-  CALL status_message(msg)
-  WRITE(msg,*)'         maximum number of i elements = ', imax
-  CALL status_message(msg)
-  WRITE(msg,*)'         maximum number of j elements = ', jmax
-  CALL status_message(msg)
-
-  ALLOCATE(species(i)%scalar(block)%conc(imin:imax,jmin:jmax), STAT = alloc_stat)		! c depth-ave concentration
-  IF(alloc_stat /= 0)THEN
-     CALL error_message('allocation failed for the concentration', fatal=.TRUE.)
-  ELSE
-     CALL status_message('allocation successful for concentration')
-  ENDIF
-  species(i)%scalar(block)%conc = 0.0
-  
-  ALLOCATE(species(i)%scalar(block)%concold(imin:imax,jmin:jmax), STAT = alloc_stat)	! c old depth-ave concentration
-  IF(alloc_stat /= 0)THEN
-     CALL error_message('allocation failed for the old concentration', fatal=.TRUE.)
-  ELSE
-     CALL status_message('allocation successful for old concentration')
-  ENDIF
-  species(i)%scalar(block)%concold = 0.0
-
-  ALLOCATE(species(i)%scalar(block)%concoldold(imin:imax,jmin:jmax), STAT = alloc_stat)	! c old depth-ave concentration
-  IF(alloc_stat /= 0)THEN
-     CALL error_message('allocation failed for the old old concentration', fatal=.TRUE.)
-  ELSE
-     CALL status_message('allocation successful for old old concentration')
-  ENDIF
-  species(i)%scalar(block)%concoldold = 0.0
-
-  ALLOCATE(species(i)%scalar(block)%srcterm(imin:imax,jmin:jmax), STAT = alloc_stat)	! c old depth-ave concentration
-  IF(alloc_stat /= 0)THEN
-     CALL error_message('allocation failed for the source term', fatal=.TRUE.)
-  ELSE
-     CALL status_message('allocation successful for source term')
-  ENDIF
-  species(i)%scalar(block)%srcterm = 0.0
-
-  ALLOCATE(species(i)%scalar(block)%cell(imin:imax,jmin:jmax), STAT = alloc_stat)	! c old depth-ave concentration
-  IF(alloc_stat /= 0)THEN
-     CALL error_message('allocation failed for cell type', fatal=.TRUE.)
-  ELSE
-     CALL status_message('allocation successful for cell type')
-  ENDIF
-  species(i)%scalar(block)%cell(:,:)%xtype = SCALAR_NORMAL_TYPE
-  species(i)%scalar(block)%cell(:,:)%xbctype = SCALBC_NONE
-  species(i)%scalar(block)%cell(:,:)%ytype = SCALAR_NORMAL_TYPE
-  species(i)%scalar(block)%cell(:,:)%ybctype = SCALBC_NONE
-
-  species(i)%scalar(block)%conc = 0.0
-  species(i)%scalar(block)%concold = 0.0
-  species(i)%scalar(block)%concoldold = 0.0
-
-  species(i)%scalar(block)%influx = 0.0
-  species(i)%scalar(block)%outflux = 0.0
-  species(i)%scalar(block)%netflux = 0.0
-  species(i)%scalar(block)%mass = 0.0
-  species(i)%scalar(block)%bedmass = 0.0
-  species(i)%scalar(block)%massold = 0.0
-  species(i)%scalar(block)%bedmassold = 0.0
-
-  WRITE(msg,*)'completed component allocation for scalars block number - ', block
-  CALL status_message(msg)
-
-END SUBROUTINE allocate_scalarblock_components
-
-!#####################################################################################################
-
-SUBROUTINE allocate_species()
-
-	IMPLICIT NONE
-	INTEGER :: alloc_stat
+    IMPLICIT NONE
+    INTEGER :: n, alloc_stat
     CHARACTER (LEN=1024) :: msg
 
-	ALLOCATE(species(max_species), STAT = alloc_stat)
-	IF(alloc_stat /= 0)THEN
+    ALLOCATE(species(max_species), STAT = alloc_stat)
+    IF(alloc_stat /= 0)THEN
        WRITE(msg,*)'allocation failed for array of species - max_species=', max_species
        CALL error_message(msg)
-	ELSE
+    ELSE
        WRITE(msg,*)'allocation successful for array of species - max_species=', max_species
        CALL status_message(msg)
-	ENDIF
+    ENDIF
 
-END SUBROUTINE allocate_species
+    DO n = 1, max_species
+       ALLOCATE(species(n)%scalar(max_blocks), STAT = alloc_stat)
+       IF(alloc_stat /= 0)THEN
+          CALL error_message('allocation failed for the array of scalars', fatal=.TRUE.)
+       ELSE
+          CALL status_message('allocation successful for array of scalars')
+       ENDIF
+    END DO
 
-!####################################################################################################
+  END SUBROUTINE allocate_species
 
-SUBROUTINE allocate_scalar(max_blocks, n)
 
-	IMPLICIT NONE
-	INTEGER :: alloc_stat, n, max_blocks, i
+  ! ----------------------------------------------------------------
+  ! SUBROUTINE allocate_scalarblock_components
+  !
+  ! this routine allocates each component in the array of blocks
+  ! allows minimal memory use for each block
+  ! ----------------------------------------------------------------
+  SUBROUTINE allocate_scalarblock_components(iblock, vbase)
+    IMPLICIT NONE
 
-	ALLOCATE(species(n)%scalar(max_blocks), STAT = alloc_stat)
-	IF(alloc_stat /= 0)THEN
-       CALL error_message('allocation failed for the array of scalars', fatal=.TRUE.)
-	ELSE
-       CALL status_message('allocation successful for array of scalars')
-	ENDIF
+#include "mafdecls.fh"
+#include "global.fh"
 
-END SUBROUTINE allocate_scalar
+    INTEGER, INTENT(IN) :: iblock
+    TYPE (block_var_base), POINTER, INTENT(INOUT) :: vbase
+
+    
+    INTEGER :: ispecies, alloc_stat, ierr(1)
+    INTEGER :: imin, imax, jmin, jmax
+
+    WRITE(msg,*)'starting component allocation for scalars block number - ', iblock
+    CALL status_message(msg)
+
+    ierr(1) = 0
+
+    DO ispecies = 1, max_species
+       species(ispecies)%scalar(iblock)%concvar => &
+            &block_var_allocate("conc", vbase, const=.FALSE.)
+       species(ispecies)%scalar(iblock)%conc => &
+            &species(ispecies)%scalar(iblock)%concvar%current
+       species(ispecies)%scalar(iblock)%concold => &
+            &species(ispecies)%scalar(iblock)%concvar%old
+       species(ispecies)%scalar(iblock)%concoldold => &
+            &species(ispecies)%scalar(iblock)%concvar%oldold
+       
+       imin = vbase%imin_owned
+       imax = vbase%imax_owned
+       jmin = vbase%jmin_owned
+       jmax = vbase%jmax_owned
+
+       ALLOCATE(species(ispecies)%scalar(iblock)%srcterm(imin:imax,jmin:jmax), STAT = alloc_stat)
+       IF (alloc_stat .NE. 0) ierr(1) = ierr(1) + 1
+
+       ALLOCATE(species(ispecies)%scalar(iblock)%cell(imin:imax,jmin:jmax), STAT = alloc_stat)
+       IF (alloc_stat .NE. 0) ierr(1) = ierr(1) + 1
+
+       CALL ga_sync()
+
+       IF (ierr(1) .EQ. 0) THEN
+          species(ispecies)%scalar(iblock)%conc = 0.0
+          species(ispecies)%scalar(iblock)%concold = 0.0
+          species(ispecies)%scalar(iblock)%concoldold = 0.0
+          
+          species(ispecies)%scalar(iblock)%cell(:,:)%xtype = SCALAR_NORMAL_TYPE
+          species(ispecies)%scalar(iblock)%cell(:,:)%xbctype = SCALBC_NONE
+          species(ispecies)%scalar(iblock)%cell(:,:)%ytype = SCALAR_NORMAL_TYPE
+          species(ispecies)%scalar(iblock)%cell(:,:)%ybctype = SCALBC_NONE
+          
+          species(ispecies)%scalar(iblock)%influx = 0.0
+          species(ispecies)%scalar(iblock)%outflux = 0.0
+          species(ispecies)%scalar(iblock)%netflux = 0.0
+          species(ispecies)%scalar(iblock)%mass = 0.0
+          species(ispecies)%scalar(iblock)%bedmass = 0.0
+          species(ispecies)%scalar(iblock)%massold = 0.0
+          species(ispecies)%scalar(iblock)%bedmassold = 0.0
+       END IF
+          
+
+    END DO
+
+    IF (ierr(1) .GT. 0) THEN
+       WRITE(msg,*)'scalars memory allocation error for block ', &
+            &iblock, ' on process ', ga_nodeid()
+       CALL error_message(msg)
+    END IF
+
+    CALL ga_igop(MT_F_INT, ierr, 1, '+');
+
+    IF (ierr(1) .GT. 0) THEN
+       CALL error_message("cannot continue", fatal=.TRUE.)
+    END IF
+    
+    WRITE(msg,*)'completed component allocation for scalars block number - ', iblock
+    CALL status_message(msg)
+
+  END SUBROUTINE allocate_scalarblock_components
+
+  ! ----------------------------------------------------------------
+  ! SUBROUTINE deallocate_species
+  ! ----------------------------------------------------------------
+  SUBROUTINE deallocate_species()
+
+    IMPLICIT NONE
+
+    INTEGER :: ispecies, iblock
+
+    DO ispecies = 1, max_species
+       DO iblock = 1, max_blocks
+          CALL block_var_deallocate(species(ispecies)%scalar(iblock)%concvar)
+          DEALLOCATE(species(ispecies)%scalar(iblock)%srcterm)
+          DEALLOCATE(species(ispecies)%scalar(iblock)%cell)
+       END DO
+       DEALLOCATE(species(ispecies)%scalar)
+    END DO
+    DEALLOCATE(species)
+
+  END SUBROUTINE deallocate_species
+
 
 END MODULE scalars
