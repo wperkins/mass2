3613b8c5df72ea520577b38a68bb48a6ce16b2cf
William Perkins <william.perkins@pnnl.gov>
2011-10-04 20:47:12 +0000

    use F90 macros in time_series/m4

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,10 +8,10 @@
 # -------------------------------------------------------------
 # -------------------------------------------------------------
 # Created March 26, 2003 by William A. Perkins
-# Last Change: Thu Jul 17 10:41:45 2003 by William A. Perkins <perk@leechong.pnl.gov>
+# Last Change: Mon Oct  3 10:15:01 2011 by William A. Perkins <d3g096@flophouse>
 # -------------------------------------------------------------
 # $Id$
 
-aclocal -I time_series  -I m4 && automake --foreign --add-missing --copy && autoconf
+aclocal -I time_series/m4 -I m4 && automake --foreign --add-missing --copy && autoconf
 
 (cd time_series; sh bootstrap.sh)
